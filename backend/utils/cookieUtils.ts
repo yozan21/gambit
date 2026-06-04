dc46d3aaa3960d6fc92df6a055f39b81f8e2cbb6
Yojan Kaphle <yojankaphle80@gmail.com>
2026-06-04 22:15:31 +0545

    Changed cookie sameSite to none for cors

--- backend/utils/cookieUtils.ts
+++ backend/utils/cookieUtils.ts
@@ -10,7 +10,7 @@ export const setAuthCookies = (
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: isProd, // HTTPS only in production
-    sameSite: "lax", // CSRF protection
+    sameSite: "none", // CSRF protection
     maxAge: 15 * 60, // 15 minutes
     path: "/",
   });
@@ -18,7 +18,7 @@ export const setAuthCookies = (
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: isProd,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60, // 7 days
     path: "/", // only sent on refresh endpoint
   });
